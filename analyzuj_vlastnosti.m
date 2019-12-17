65f2db2fe16ce0eaab916d20b7010e26bbbb85f4
Rudolf Trautenberger <xtrautenberger@stuba.sk>
2019-12-17 10:28:27 +0100

    Verzia 1.0

---
+++ analyzuj_vlastnosti.m
@@ -0,0 +1,7 @@
+function analyzuj_vlastnosti(cit,men)
+if (length(cit) <= length(men))
+    disp('System je fyzikalne realizovatelny.')
+else
+    disp('System nie je fyzikalne realizovatelny!')
+end
+end
---
+++ testuj.m
@@ -0,0 +1,9 @@
+% FN
+cit=[1 1 1];
+men=[1 1];
+analyzuj_vlastnosti(cit,men)
+
+% FR
+cit=[1 1];
+men=[1 1 1];
+analyzuj_vlastnosti(cit,men)
